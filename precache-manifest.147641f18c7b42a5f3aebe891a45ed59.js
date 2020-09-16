686ef211d4e35b1dabb5845211def0b0d5f4a7aa
dipsywong98 <dipsywong98@users.noreply.github.com>
2020-09-16 00:36:08 +0000

    deploy: 9b4e5979db308dc4778dc7debc4791ebf1a101ef

---
+++ precache-manifest.147641f18c7b42a5f3aebe891a45ed59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43128cf17888f5f18cf4e18b80e6439f",
+    "url": "/photosynthesis/index.html"
+  },
+  {
+    "revision": "55fbdf84a280bb4ca0b0",
+    "url": "/photosynthesis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9521ab0ffdce76e4e2f4",
+    "url": "/photosynthesis/static/js/2.8b1d63ce.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/photosynthesis/static/js/2.8b1d63ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fbdf84a280bb4ca0b0",
+    "url": "/photosynthesis/static/js/main.849c4458.chunk.js"
+  },
+  {
+    "revision": "afeb360bc5de0e0bb7b4",
+    "url": "/photosynthesis/static/js/runtime-main.65766f86.js"
+  }
+]);
