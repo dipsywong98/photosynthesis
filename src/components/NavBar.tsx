cae8889faee5bf8038e93dcc7b3d6ba325559d54
dipsywong98 <ycwongal@connect.ust.hk>
2020-10-01 22:29:42 +0800

    fix navbar display

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,12 +13,15 @@ const propTypes = {
   state: PropTypes.number.isRequired
 }
 
-const NavBar: FunctionComponent<PropTypes.InferProps<typeof propTypes>> = ({ state, setState }) => {
+const NavBar: FunctionComponent<PropTypes.InferProps<typeof propTypes>> = () => {
   const [colorMode, setColorMode] = useColorMode()
   const toggleDarkMode = (): void => setColorMode(colorMode === 'default' ? 'dark' : 'default')
 
   return (
-    <Flex py={2} px={theme.layout.container.p} sx={{ alignItems: 'center', position: 'fixed', width: '100%' }}>
+    <Flex
+      py={2}
+      px={theme.layout.container.p}
+      sx={{ alignItems: 'center', position: 'fixed', width: '100%', zIndex: 1 }}>
       <Flex mr={2} sx={{ alignItems: 'center', flex: 1 }}>
         {/* {state > 0 && <Button onClick={() => setState(state - 1)}><Icon path={mdiArrowLeft}/></Button>} */}
       </Flex>
