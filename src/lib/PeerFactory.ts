f66135d1541276bfaeb6ca3dd5cab93ccbf89f9a
danvim <daniel.2345@hotmail.com>
2020-10-18 04:08:48 +0800

    Try fix WebRTC

--- src/lib/PeerFactory.ts
+++ src/lib/PeerFactory.ts
@@ -12,11 +12,14 @@ export const PeerFactory: { peers: Peer[], useFake: boolean, make: (id?: string)
       if (process.env.REACT_APP_PEER_HOST === undefined) {
         return new Peer(id)
       } else {
+        console.log(process.env)
         return new Peer(id, {
           host: process.env.REACT_APP_PEER_HOST ?? 'localhost',
           port: Number.parseInt(process.env.REACT_APP_PEER_PORT ?? '9000'),
           path: process.env.REACT_APP_PEER_PATH ?? '/peer',
-          secure: process.env.REACT_APP_PEER_SECURE === 'true'
+          secure: process.env.REACT_APP_PEER_SECURE === 'true',
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          config: typeof process.env.REACT_APP_PEER_CONFIG === 'string' ? JSON.parse(process.env.REACT_APP_PEER_CONFIG) : undefined
         })
       }
     } else {
